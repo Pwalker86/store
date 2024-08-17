ed74a7d760755447049776896d9a9ec5556a69d3
Phil Walker <phil1586@gmail.com>
2024-08-17 07:55:59 -0600

    use order item quantity increase/decrease order items on orders#show

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,15 +1,32 @@
 class OrderItemsController < ApplicationController
   def create
     @product = Product.find(params[:product_id])
-    @order_item = @order.order_items.create(product: @product)
+    @order_item = @order.order_items.find_or_create_by(product: @product)
+    @order_item.quantity += params[:qty].to_i
 
     if @order_item.save
-      redirect_to @product, notice: 'Product was successfully added to your order.'
+      redirect_to @order, notice: 'Product was successfully added to your order.'
     else
-      redirect_to @product, alert: 'There was an error adding the product to your order.'
-    end  end
+      redirect_to @order, alert: 'There was an error adding the product to your order.'
+    end
+  end
 
   def update
+    @product = Product.find(params[:product_id])
+    @order_item = @order.order_items.find_by(product: @product)
+    if @order_item
+      # TODO: if quantity will go to 0, destroy the order_item
+      @order_item.quantity += params[:qty].to_i
+
+      if @order_item.save!
+        redirect_to @order, notice: 'Quantity was successfully updated.'
+      else
+        redirect_to @order, alert: 'There was an error adding the product to your order.'
+      end
+    else
+      redirect_to @order, alert: 'There was an error adding the product to your order.'
+    end
+
   end
 
   def destroy
--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,8 @@ class Order < ApplicationRecord
   belongs_to :user
   has_many :order_items
   has_many :products, through: :order_items
+
+  def order_item_totals
+    order_items.sum(:quantity)
+  end
 end
