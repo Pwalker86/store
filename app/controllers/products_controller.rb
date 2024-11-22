b8735298513dda369698c6d8db7150e47c049f7c
Phil Walker <phil1586@gmail.com>
2024-11-22 09:56:49 -0700

    working on turbo frames for edit/show

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,6 +9,10 @@ class ProductsController < ApplicationController
 
   # GET /products/1 or /products/1.json
   def show
+    respond_to do |format|
+      format.html { render :show }
+      format.turbo_stream
+    end
   end
 
   # GET /products/new
