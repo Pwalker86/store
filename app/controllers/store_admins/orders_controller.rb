689bf87a738915cfbabf8d2b67f0181474463bad
Phil Walker <phil1586@gmail.com>
2024-12-16 16:16:51 -0700

    scope orders for store admins
    When a store admin views an order, the order items are scoped to only
    the products that the store admin owns

--- app/controllers/store_admins/orders_controller.rb
+++ app/controllers/store_admins/orders_controller.rb
@@ -5,5 +5,10 @@ module StoreAdmins
     def index
       @orders = OrderDecorator.decorate_collection(current_store_admin.store_orders)
     end
+
+    def show
+      @order = Order.find(params[:id])
+      @scoped_order_items = @order.order_items.find_all { |item| item.product.store_id === current_store_admin.store.id }
+    end
   end
 end
